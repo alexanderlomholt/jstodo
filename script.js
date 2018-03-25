01f0a569047dcd3cbdf514f702eac4dd3a28c2b3
Alexander Lomholt <arlomholt@gmail.com>
2018-03-25 22:32:24 +0200

    added toggle complete and remove items

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-
-
+// Ading new items to list
 const input = document.querySelector('input');
 
+const addItem = () => {
+  let ul = document.querySelector('ul');
+  let string = `<li><span class="todo-text" onclick="complete(this)">${input.value}</span><i class="far fa-trash-alt" onclick="removeItem(this)"></i></li>`;
+  ul.insertAdjacentHTML('beforeend', string);
+  input.value = '';
+}
+
 input.addEventListener('keyup', (e) => {
-
   if (e.key == 'Enter') {
-    let ul = document.querySelector('ul');
-    ul.insertAdjacentHTML('beforeend', `<li>${input.value}</li>` );
-    input.value = '';
+    addItem();
   }
-
-
 });
 
+// Marking items as complete/not complete
+const complete = (item) => {
+  item.classList.toggle('complete');
+}
 
-
-
-
+// Deleting items from list
+const removeItem = (item) => {
+  item.parentNode.parentNode.removeChild(item.parentNode);
+}
 
 
 
